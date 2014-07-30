b5c5dc1bded9c7d566d81d914ced9236164e34c1
Matt Brophy <matt@brophy.org>
2014-07-30 10:36:42 -0400

    Moved to handlebars templates for the validation screens and wired up a mock ajax call to the endpoint

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
-var UCE = window.UCE || {};
+var UCE = window.UCE || {},
+    Handlebars = window.Handlebars;
 
 // From: http://bit.ly/1gAKKPP
 function isInPhoneGap() {
@@ -25,6 +26,7 @@ UCE.init = function () {
 
 UCE.bindListeners = function () {
   $('.btn-scan').on('click', UCE.scanTicket);
+  $('.btn-submit').on('click', UCE.submitManualCode);
   $('.hide').on('click', UCE.reset);
 };
 
@@ -81,6 +83,69 @@ UCE.scanTicket = function () {
   }
 
   return dfd.promise();
+};
+
+UCE.submitManualCode = function (e) {
+  var $code = $('.input-qrcode'),
+      code = $code.val();
+
+  if (code.trim() === '') {
+    UCE.log('Invalid code entered');
+    window.alert('Please enter a valid Ticket Code');
+    return;
+  }
+
+  UCE.submitTicket(code);
+};
+
+UCE.submitTicket = function (code) {
+  var dfd = new $.Deferred();
+
+  setTimeout(function () {
+    var mockData;
+
+    if (Math.random() < 0.1) {
+      return dfd.reject();  // Mimic failed ajax request
+    } else if (Math.random() < 0.55) {
+      mockData = {
+        valid: true,
+        ticketCode: code,
+        ticketType: 'VIP',
+        attendeeName: 'John Doe'
+      };
+    } else {
+      mockData = {
+        valid: false,
+        ticketCode: code,
+        error: 'Ticket already redeemed'
+      };
+    }
+
+    dfd.resolve(mockData);
+  }, 1000);
+
+  function success(data) {
+    var source, template;
+    if (data.valid) {
+      source = $('#tpl-valid').html();
+      template = Handlebars.compile(source);
+      $('.page-valid .content').html(template(data));
+      return UCE.showValid();
+    } else {
+      source = $('#tpl-invalid').html();
+      template = Handlebars.compile(source);
+      $('.page-invalid .content').html(template(data));
+      return UCE.showInvalid();
+    }
+  }
+
+  function error(e) {
+    UCE.log('Could not submit ticket');
+    window.alert('Ticket could not be submitted.  Please make sure you have ' +
+                 'a valid internet connection and try again.');
+  }
+
+  return dfd.promise().then(success, error);
 };
 
 UCE.deviceReadyDfd.promise().done(UCE.init);
